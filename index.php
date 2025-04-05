2ec64e6c703acd7f77378cab6d9c2d8eace432de
Romell <jromell099@gmail.com>
2025-04-05 19:41:11 +0800

    first commit

---
+++ index.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <link rel="stylesheet" type="text/css" href="styles.css">
+    <title>Document</title>
+</head>
+<body>
+    
+<nav class="shadow navbar custom-navbar sticky-top">
+      <div class="container-fluid">
+        <a class="navbar-brand d-flex align-items-center" href="index.php">
+          <img src="assets/logo.png" width="45" height="45" class="d-inline-block align-middle me-2">
+          FOUND-IT!
+        </a>
+        <ul class="navbar-nav flex-row flex-wrap bd-navbar-nav">
+          <li nav-item col-6 col-lg-auto>
+            <a class="navbar-brand d-flex align-items-center" href="">Database</a>
+          </li>
+          <li nav-item col-6 col-lg-auto>
+            <a class="navbar-brand d-flex align-items-center" href="Forms/reportform.php">Report Item</a>
+          </li>
+      </div>
+</nav>
+
+    <h1>TEST</h1>
+</body>
+</html>
